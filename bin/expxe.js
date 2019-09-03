54d08d294c2941585d53a9d9d19e5f1d65d0368b
lixuefeng <jrelt@163.com>
2019-09-03 09:57:15 +0800

    init project

---
+++ bin/expxe.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk')
+const program = require('commander')
+const { cleanArgs } = require('../lib/utils')
+const create = require('../lib/create')
+const init = require('../lib/init')
+const init = require('../lib/icon')
+
+program.version(require('../package').version).usage('<command> [options]')
+
+program
+  .command('create <project-name>')
+  .description('create a new project powered by expxe-cli')
+  .option(
+    '-p, --preset <presetName>',
+    'Skip prompts and use saved or remote preset'
+  )
+  .option('-d, --default', 'Skip prompts and use default preset')
+  .option(
+    '-i, --inlinePreset <json>',
+    'Skip prompts and use inline JSON string as preset'
+  )
+  .action((name, cmd) => {
+    create(name, cleanArgs(cmd))
+  })
+
+program
+  .command('init <template> <poject-name>')
+  .description('generate a project from a remote template')
+  .option(
+    '-p, --preset <presetName>',
+    'Skip prompts and use saved or remote preset'
+  )
+  .option('-d, --default', 'Skip prompts and use default preset')
+  .option(
+    '-i, --inlinePreset <json>',
+    'Skip prompts and use inline JSON string as preset'
+  )
+  .action((template, name, cmd) => {
+    init(template, name, cleanArgs(cmd))
+  })
+
+program
+  .command('icon <svg> <dist>')
+  .description('generate vue component or font from svg')
+  .option('-c, --componet', 'generate vue components')
+  .option('-f, --font [fontName]', 'generate  icon font')
+  .option('-a, --add', 'add new icon')
+  .action((svg, dist, cmd) => {
+    icon(svg, dist, cleanArgs(cmd))
+  })
